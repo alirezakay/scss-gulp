78ffa61fd84438aaedec324555b537d4aa69dbed
Alireza Kavian <kay.alireza@gmail.com>
2019-01-22 21:44:55 +0330

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,24 +9,25 @@ var changed = require('gulp-changed');
 
 
 
-var SCSS_SRC = './src/assets/scss/**/*.scss';
+var SCSS_SRC = './src/assets/scss/*.scss';
 var SCSS_DEST = './src/assets/css';
 
 // Compile SCSS
-gulp.task('compile_scss', function(){
-
+gulp.task('compile_scss', function(cb){
     gulp.src(SCSS_SRC) 
     .pipe(sass().on('error', sass.logError)) 
     .pipe(minifyCSS())
     .pipe(rename({ suffix: '.min' })) 
     .pipe(changed(SCSS_DEST)) 
     .pipe(gulp.dest(SCSS_DEST));
+    cb();
 });
 
 
 //detect changes in SCSS
-gulp.task('watch_scss', function(){ 
+gulp.task('watch_scss', function(cb){ 
     gulp.watch(SCSS_SRC, gulp.series('compile_scss'));
+    cb();
 });
 
 
